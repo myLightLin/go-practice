b3bb3dd5f98d6aebe0e8da005e6ec027fb31857e
linguangliang <linguangliang@ifanr.com>
2021-11-13 12:13:01 +0800

    add exercise5.19

---
+++ ch5/exercise5.19/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(returnNoZero())
+}
+
+func returnNoZero() (result int) {
+	defer func() {
+		result = 3
+		_ = recover()
+	}()
+	panic("panic!")
+}
---
+++ ch5/title3/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+
+}
+
+func soleTitle(doc *html.Node) (title string, err error) {
+	type bailout struct{}
+
+	defer func() {
+		switch p := recover(); p {
+		case nil:
+		case bailout{}:
+			err = fmt.Errorf("mutiple title elements")
+		default:
+			panic(p)
+		}
+	}()
+
+	forEachNode(doc, func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "title" && n.FirstChild != nil {
+			if title != "" {
+				panic(bailout{})
+			}
+			title = n.FirstChild.Data
+		}
+	}, nil)
+
+	if title == "" {
+		return "", fmt.Errorf("no title element")
+	}
+
+	return title, nil
+}
