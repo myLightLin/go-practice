275a8c6337e9262c9be237a1e42a0db731cc871e
linguangliang <linguangliang@ifanr.com>
2021-12-22 22:50:20 +0800

    add new exercise

---
+++ ch11/storage2/quota_test.go
@@ -0,0 +1,30 @@
+package storage
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestCheckQuotaNotifiesUser(t *testing.T) {
+	var notifiedUser, notifiedMsg string
+	notifiedUser = func(user, msg string) {
+		notifiedUser, notmnotifiedMsg = user, msg
+	}
+
+	const user = "test@example.org"
+	usage[user] = 980000000
+
+	CheckQuota(user)
+	if notifiedUser == "" && notifiedMsg == "" {
+		t.Fatalf("notifyUser not called")
+	}
+	if notifiedUser != user {
+		t.Errorf("wrong user (%s) notified, want %s",
+			notifiedUser, user)
+	}
+	const wantSubstring = "98% of your quota"
+	if !strings.Contains(notifiedMsg, wantSubstring) {
+		t.Errorf("unexpected notification message <<%s>>, "+
+			"want substring %q", notifiedMsg, wantSubstring)
+	}
+}
