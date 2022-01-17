f867d2d8732000c750a98dbf12151bccabe13611
linguangliang <linguangliang@ifanr.com>
2022-01-17 22:14:47 +0800

    add new dev

---
+++ go-in-action/chapter5/listing.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type user struct {
+	name  string
+	email string
+}
+
+func (u user) notify() {
+	fmt.Printf("Sending User Email to %s<%s>\n", u.name, u.email)
+}
+
+func (u *user) changeEmail(email string) {
+	u.email = email
+}
+
+func main() {
+	bill := user{"Bill", "bill@email.com"}
+	bill.notify()
+
+	lisa := &user{"Lisa", "lisa@email.com"}
+	lisa.notify()
+
+	bill.changeEmail("bill@newdomain.com")
+	bill.notify()
+
+	lisa.changeEmail("lisa@newdomain.com")
+	lisa.notify()
+}
